200a6e34a0ed3f32e93e6870254d27c0002b0b8e
steve3github <badayosethan@gmail.com>
2023-06-04 12:04:52 +0800

    new clamsses

---
+++ Item.java
@@ -0,0 +1,42 @@
+public class Item {
+    private String name;
+    private float price;
+    private int stock;
+
+    public Item(String name, float price, int stock){
+        this.name = name;
+        this.price = price;
+        this.stock =stock;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    private boolean isAvailable(){
+        if(stock > 0)
+            return true;
+        else
+            return false;
+    }
+}
