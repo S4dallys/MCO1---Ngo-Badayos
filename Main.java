ebeda0673a4eaa751f806436ee6de1de2bb528ca
S4dallys <geometrygaming8@gmail.com>
2023-06-12 20:48:12 +0800

    random

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    start();
+  }
+
+  public static void start() {
+
+  }
+
+  private static void test() {
+
+  }
+
+  private static void doAction() {
+  }
+}
