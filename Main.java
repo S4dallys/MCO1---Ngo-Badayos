9ee9e3e400ff7384160677f898bbd3898d9aebc3
S4dallys <geometrygaming8@gmail.com>
2023-06-13 08:53:10 +0800

    configured start()

--- Main.java
+++ Main.java
@@ -1,18 +1,51 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    start();
-  }
+    public static void main(String[] args) {
+        start();
+    }
 
-  public static void start() {
+    public static void start() {
+        Scanner sc = new Scanner(System.in);
+        int choice = 1;
 
-  }
+        while (choice != 3) {
+            System.out.println("1. Create Vending Machine");
+            System.out.println("2. Test Vending Machine");
+            System.out.println("3. Exit");
 
-  private static void test() {
+            choice = sc.nextInt();
+            sc.nextLine(); // catch \n
 
-  }
+            if (choice == 1) {
 
-  private static void doAction() {
-  }
+            }
+            else if (choice == 2) {
+                test();
+            }
+            else if (choice == 3) {
+                continue;
+            }
+            else {
+                // error message
+            }
+        }
+
+        sc.close(); // close scanner
+    }
+
+    private static void test() {
+
+    }
+
+    private static void testVending() {
+
+    }
+
+    private static void testMaintenance() {
+
+    }
+
+    private static void doAction() {
+    }
 }
