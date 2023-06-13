fcb649867b85bf924b7a737dbf85899d5b680c2c
steve3github <badayosethan@gmail.com>
2023-06-13 09:15:04 +0800

    lol

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    start();
-  }
+    public static void main(String[] args) {
+        start();
+    }
 
-  public static void start() {
+    public static void start() {
 
-  }
+    }
 
-  private static void test() {
+    private static void test() {
 
-  }
+    }
 
-  private static void doAction() {
-  }
+    private static void doAction() {
+    }
 }
